b546161cdb8eef4507daed80da51d3039f29cbc6
Mike <mike@mikedonovan.dev>
2023-02-02 12:46:12 -0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './css/styles.css';
+import { pause } from './util.js';
+
+async function renderOnTerminal(textLine, speed=40) {
+  let screen = document.getElementById('terminal');
+  let letterArray = textLine.split('');
+  let newLine = document.createElement('div');
+  newLine.classList.add('terminal-line')
+  screen.append(newLine);
+  for (const char of letterArray) {
+    newLine.innerHTML += char;
+    await pause(speed);
+  }
+}
+
+// renderOnTerminal('I have a brass waterbed')
